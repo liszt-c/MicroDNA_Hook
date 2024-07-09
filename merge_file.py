686f21e5362de4d86a2f9de7eb945e826fe09a78
李治郅 <65268347+liszt-c@users.noreply.github.com>
2024-07-09 13:50:59 +0800

    Fix merge_file.py

--- merge_file.py
+++ merge_file.py
@@ -10,23 +10,25 @@ ATCG
 3.循环读取glob文件，先写'>名称'
 4.写入文件内容
 '''
+# 2024年7月9日 修正生成的fa序列格式
 # -*- coding: UTF-8 -*-
 import os
 import glob
 
 
 count = 0
-ecc_path = glob.glob(r'./cnvkit_do//fa/*_*_*.txt')
-
+ecc_path = glob.glob(r'.//cnvkit_do//fa//*_*_*.txt')
+num_all  = len(ecc_path)
 if os.path.exists('.//cnvkit_do//connect') == False:
     cmd2 = os.system('mkdir .//cnvkit_do//connect')
 file = open(".//cnvkit_do//connect//all_ecc.fa",'w')
 for i in ecc_path:
     count = count + 1
-    write_line1 = '>'+str(count)
+    write_line1 = '>num'+str(count)
     file.writelines(write_line1+'\n')
     with open(i, 'r') as f:
-        read_DNA = f.read()
-        file.write(read_DNA+'\n')
+        read_DNA = f.read().replace('\n', '').replace('\r', '')
+        file.write(read_DNA + '\n')
+    print(count,'/',num_all)
 file.close()
 print(count)
